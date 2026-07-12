8019cbd7459610729c20a851603b59807111ddca
laurent bernabé <laurent.bernabe@gmail.com>
2026-07-12 13:13:29 +0200

    feat: we can set dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rich_chess_notes/i18n/strings.g.dart';
 import 'package:rich_chess_notes/pages/home.dart';
+import 'package:rich_chess_notes/providers/dark_theme_provider.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,15 +11,19 @@ void main() async {
   runApp(ProviderScope(child: TranslationProvider(child: const MainApp())));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final inDarkMode = ref.watch(darkThemeProvider);
     return MaterialApp(
       locale: TranslationProvider.of(context).flutterLocale,
       debugShowCheckedModeBanner: false,
       home: Scaffold(body: HomeWidget()),
+      theme: FlexThemeData.light(scheme: FlexScheme.greenM3),
+      darkTheme: FlexThemeData.dark(scheme: FlexScheme.blueWhale),
+      themeMode: inDarkMode ? ThemeMode.dark : ThemeMode.light,
     );
   }
 }
